82b13a2f6cc0272ababc9b8851ea57d47f3e42e6
huxian <754474542@qq.com>
2023-06-06 20:20:52 +0800

    2023-6-6晚自习结束

---
+++ clock/node.js
@@ -0,0 +1,53 @@
+const fs = require('fs')
+const path = require('path')
+const resstyle = /<style>[\s\S]*<\/style>/
+const resScript = /<script>[\s\S]*<\/script>/
+fs.readFile(path.join(__dirname, 'index.html'), 'utf-8', function (err, data) {
+    if (err) {
+        return console.log(err.message)
+    }
+    resolveCSS(data)
+    resolveJS(data)
+    resolveindex(data)
+})
+
+function resolveCSS(html_str) {
+    const css = resstyle.exec(html_str)
+    css[0] = css[0].replace('<style>', '').replace('</style>', '')
+    fs.writeFile(path.join(__dirname, '/index.css'), css[0], 'utf-8', function (err) {
+        if (err) {
+            return console.log(err.message)
+        }
+        console.log('文件css提取成功')
+    })
+}
+
+function resolveJS(html_str) {
+    const js = resScript.exec(html_str)
+    js[0] = js[0].replace('<script>', '').replace('</script>', '')
+    fs.writeFile(path.join(__dirname, '/index1.js'), js[0], 'utf-8', function (err) {
+        if (err) {
+            return console.log(err.message)
+        }
+        console.log('文件js提取成功')
+    })
+}
+
+function resolveindex(html_str) {
+
+    const index_str = html_str
+        .replace(resstyle, '<link href="index.css" rel="stylesheet">')
+        .replace(resScript, '<script src="index1.js"></script>')
+
+    fs.writeFile(path.join(__dirname, '/index.html'), index_str, function (err) {
+        if (err) {
+            return console.log(err.message)
+        }
+
+        console.log('文件index修改完成')
+
+    })
+
+}
+
+
--- node.js
+++ node.js
@@ -1,13 +1,26 @@
-const fs = require('fs')
-const path = require('path')
 const http = require('http')
+const fs = require("fs");
+const path = require("path");
+const service = http.createServer()
 
-const server = http.createServer()
-
-server.on('request',function (req, res){
-    console.log('你成功的访问到了服务器，且触发了request事件')
+service.on('request', (req, res) => {
+    const url = req.url
+    let conent = `<h1>404 Not Found</h1>`
+    if(url === '/' || url === '/index.html'){
+        conent = `<h1>这个是主页</h1>`
+    }else if(url === '/about.html'){
+        conent = `<h1>关于页面</h1>`
+    }else if (url === 'click/index.html'){
+        console.log(1)
+         fs.readFile(path.join(__dirname,'/clock/index.html'),function (err, data){
+             if(err) return console.log(err.message)
+             conent = data
+        })
+    }
+    res.setHeader('Content-Type','text/html;charset=utf-8')
+    res.end(conent )
 })
 
-server.listen(80,function (){
-    console.log('80端口本地服务器已启动。')
+service.listen(80, function () {
+    console.log('80技师为您服务,门牌号: http://127.0.0.1')
 })
