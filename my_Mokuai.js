467a721731d0d0cf30561249806cc884d82a4f2f
huxian <754474542@qq.com>
2023-06-07 10:45:43 +0800

    这里是node服务器输入/就指定其他的路径的案例

---
+++ my_Mokuai.js
@@ -0,0 +1,4 @@
+console.log('你成功加载了my_Mokuai.js')
+
+
+
--- node.js
+++ node.js
@@ -1,26 +1,27 @@
 const http = require('http')
-const fs = require("fs");
-const path = require("path");
+const path = require("path")
+const fs = require("fs")
+
 const service = http.createServer()
 
-service.on('request', (req, res) => {
+service.on('request',function (req, res){
     const url = req.url
-    let conent = `<h1>404 Not Found</h1>`
-    if(url === '/' || url === '/index.html'){
-        conent = `<h1>这个是主页</h1>`
-    }else if(url === '/about.html'){
-        conent = `<h1>关于页面</h1>`
-    }else if (url === 'click/index.html'){
-        console.log(1)
-         fs.readFile(path.join(__dirname,'/clock/index.html'),function (err, data){
-             if(err) return console.log(err.message)
-             conent = data
-        })
+    let fpath = ''
+    if(url === '/'){
+        fpath = path.join(__dirname,'/clock/index.html')
+    }else {
+        fpath = path.join(__dirname,'/clock',url)
     }
+
     res.setHeader('Content-Type','text/html;charset=utf-8')
-    res.end(conent )
+    console.log(fpath)
+    fs.readFile(fpath,'utf-8',function (err, data){
+        if(err) return res.end(`<h1>404咯</h1>`)
+        res.end(data)
+    })
+
 })
 
-service.listen(80, function () {
-    console.log('80技师为您服务,门牌号: http://127.0.0.1')
+service.listen(80,function (){
+    console.log('80技师为您服务，门牌号:http://127.0.0.1')
 })
