a1d3fb1f45e2849798d73d3a48808a473f3655df
huxian <754474542@qq.com>
2023-06-07 17:20:28 +0800

    使用moment包初始化时间

--- node.js
+++ node.js
@@ -1,6 +1,9 @@
 const my_Mokuai = require('./my_Mokuai')
+const moment = require('moment')
 
-console.log(my_Mokuai.dataFormat(new Date()))
+console.log(moment().format('YYYY-MM-HH hh:mm:ss'))
+
+
 
 
 
